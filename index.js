3da6ed2d13a9b2ba5e3fd787cec8537e2687b433
sofiagalkina <gal18024@byui.edu>
2025-02-11 22:14:39 -0700

    -

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* Maps
 function solution(scores){
 
     let frequency = new Map()
@@ -13,3 +14,27 @@ function solution(scores){
     
     
     console.log(solution([100, 90, 90, 80, 70, 70, 60]))
+    */
+
+    // queues and stuff
+    function solution(heights, k){
+        let queue = [[0, 0]] // start w index 0 and 0 jumps
+        let visited = new Set();
+
+        while(queue > 0){
+            let [currentIndex, jumps ] = queue.shift();
+            if(currentIndex == heights.length - 1){
+                return jumps;
+            }
+            for(let nextIndex = currentIndex + 1; nextIndex < heights.length; nextIndex++){
+                if(Math.abs(heights[currentIndex] - heights[nextIndex]) <= k &&
+            !visited.has(nextIndex)){
+                queue.push([nextIndex, jumps + 1]);
+                visited.add(nextIndex);
+            }
+            }
+        }
+        return -1
+    }
+
+    console.log(solution([1, 3, 2, 6, 4, 5], 2))
